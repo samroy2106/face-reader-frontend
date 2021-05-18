1bb4156f4e82e9c2f91213206819b12a9acacb99
Samarth Roy <samarthroy2106@gmail.com>
2021-05-18 20:55:40 +0530

    Changed localhost to Heroku endpoints

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://frozen-cove-72486.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
